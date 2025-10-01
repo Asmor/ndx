9290c4feb017e7692dd4fb1f07471d727762f520
Ian Toltz <itoltz@gmail.com>
2025-10-01 11:45:28 -0400

    Making icon paths relative

--- src/util/icons.ts
+++ src/util/icons.ts
@@ -7,19 +7,19 @@ interface Icons {
 
 const icons: Icons = {
   action: {
-    attack: "/icons/action/attack.png",
-    boost: "/icons/action/boost.png",
-    defend: "/icons/action/defend.png",
-    hinder: "/icons/action/hinder.png",
-    overcome: "/icons/action/overcome.png",
-    recover: "/icons/action/recover.png",
+    attack: "icons/action/attack.png",
+    boost: "icons/action/boost.png",
+    defend: "icons/action/defend.png",
+    hinder: "icons/action/hinder.png",
+    overcome: "icons/action/overcome.png",
+    recover: "icons/action/recover.png",
   },
   die: {
-    d4: "/icons/die/d4.png",
-    d6: "/icons/die/d6.png",
-    d8: "/icons/die/d8.png",
-    d10: "/icons/die/d10.png",
-    d12: "/icons/die/d12.png",
+    d4: "icons/die/d4.png",
+    d6: "icons/die/d6.png",
+    d8: "icons/die/d8.png",
+    d10: "icons/die/d10.png",
+    d12: "icons/die/d12.png",
   },
 };
 
